366a4234f028bf06c79d8ae952552b69f01cf732
Sergii Syrkin <ssyrkin@yandex.ru>
2018-05-22 16:27:00 +0300

    Adding test for rendering App

--- src/blocks/App/App.test.js
+++ src/blocks/App/App.test.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
+import Messaging from '../Messaging/Messaging';
 
-it('renders welcome message', () => {
+it('renders App with Messaging component', () => {
     const wrapper = shallow(<App />);
     const welcome = <header className='app__header'>
         <h1 className='app__title'>Welcome to simple log messages</h1>
     </header>;
 
     expect(wrapper.contains(welcome)).toEqual(true);
+    expect(wrapper.contains(<Messaging/>)).toEqual(true);
 });
